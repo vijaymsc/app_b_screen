f7083f353ac04031f90552b1182ad396ada43c8a
vijaymsc <vijayvsjc15@gmail.com>
2025-07-16 17:48:39 +0530

    initial commit

---
+++ lib/app_b_module.dart
@@ -0,0 +1,8 @@
+library app_b_module;
+export 'src/app_b_screen.dart';
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
---
+++ lib/src/app_b_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AppBScreen extends StatelessWidget {
+  const AppBScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('App B Screen')),
+      body: const Center(
+        child: Text(
+          'This is App B\'s screen!',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
